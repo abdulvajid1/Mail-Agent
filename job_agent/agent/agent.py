2eca8586203d6f8193630c4dcc9fe46ec4c25f40
abdulvajid1 <vajidca444@gmail.com>
2026-07-05 20:37:37 +0530

    packaging

--- job_agent/agent/agent.py
+++ job_agent/agent/agent.py
@@ -1,7 +1,7 @@
 from langchain_core.messages import (SystemMessage, 
                                      HumanMessage)
 from langchain_ollama import ChatOllama
-from prompts import SYSTEM_PROMPT #type: ignore
+from job_agent.prompts.system_prompt import SYSTEM_PROMPT #type:
 
 
 
@@ -11,21 +11,24 @@ def main():
         SystemMessage(content=SYSTEM_PROMPT)
     ]
     
-    llm = ChatOllama(model="llama3.1", reasoning=True)
+    llm = ChatOllama(model="llama3.1")
     
-    user_input = ""
+    user_input = input("Ask something")
     # make call, if tool needed use the tool func else give output to user
     while user_input != "exit":
-        messages.extend([HumanMessage(content=user_input)])
+        messages.extend([HumanMessage(content=user_input)]) #type: ignore
+        for msg in messages:
+            msg.pretty_print()
+            
         llm_output = llm.stream(messages)
         
         for i in llm_output:
-            print(i)
+            print(i.content, end="", flush=True)
         
         new_user_input = input('What to ask next')
         user_input = new_user_input
 
 
 if __name__ == "__main__":
-    print("Starting")
+    print("Starting the AGENT....")
     main()
