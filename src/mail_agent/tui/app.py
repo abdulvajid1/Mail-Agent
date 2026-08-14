d2fea92d06f92ff7b4eebe8ce07fdb099ef195d6
abdulvajid1 <vajidca444@gmail.com>
2026-08-14 11:05:24 +0530

    Add tui with chatbox

---
+++ src/mail_agent/tui/app.py
@@ -0,0 +1,26 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Static, Header, Footer
+from textual.containers import Horizontal
+from textual.widgets import Input, Static
+class MailAgent(App):
+    """
+    The TUI for Mail Agent.
+    """
+
+    TITLE = "Mail Agent"
+    CSS_PATH = "./app.tcss"
+
+    def compose(self):
+        yield Header(show_clock=True)
+        with Horizontal(id="input_box"):
+            yield Static(">", id="prompt_icon")
+            yield Input(
+                placeholder="Ask me any Mail related questions",
+                id="prompt_input"
+                )
+        yield Footer()
+
+
+if __name__ == "__main__":
+    app = MailAgent()
+    app.run()
